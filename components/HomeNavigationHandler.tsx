81bd7f5eae934b478fb7c60c490bb72851ac8c88
Christopher Kelvin <christopherkelvin100@gmail.com>
2025-03-22 17:38:50 +0300

    Before removing the profile tab

--- components/HomeNavigationHandler.tsx
+++ components/HomeNavigationHandler.tsx
@@ -3,14 +3,27 @@ import React from 'react';
 import { HomeScreen } from 'screens/homeScreen';
 import { SettingScreen } from 'screens/settingScreen';
 
+import colors from '../constants/colors';
 const Stack = createStackNavigator();
 
 export function HomeNavigationHandler() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Setting" component={SettingScreen} />
+      <Stack.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          title: 'Settings',
+          headerStyle: {
+            backgroundColor: colors.main,
+          },
+          headerTintColor: colors.secondary,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
-//  screenOptions={{ headerShown: false }}
